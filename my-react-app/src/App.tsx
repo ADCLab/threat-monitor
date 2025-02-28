a0f4fcdf6a6026468d4a1eed6abe8107dd92bc56
cam-ucf <ca840220@ucf.edu>
2025-02-27 20:17:03 -0500

    replace with react app

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Survey from './Components/Survey';
+
+function App() {
+  return (
+    <div className="App">
+      <Survey />
+    </div>
+  );
+}
+export default App;
---
+++ my-react-app/src/Components/Survey.tsx
@@ -0,0 +1,68 @@
+function Survey() {
+
+  const submit = () => {
+    console.log("Submitted");
+    const radios = document.getElementsByName("rank");
+    let rank = 0;
+    // check which radio button is checked
+
+
+
+    for (let i = 0; i < radios.length; i++) {
+      if ((radios[i] as HTMLInputElement).checked) {
+        rank = parseInt((radios[i] as HTMLInputElement).value);
+
+
+        break;
+
+      }
+    }
+
+    if (rank === 0) {
+      alert("Please select a rating");
+      return;
+    }
+
+    // clear the radio buttons
+    for (let i = 0; i < radios.length; i++) {
+      (radios[i] as HTMLInputElement).checked = false;
+    }
+    console.log("Rank: ", rank);
+
+  }
+  return (
+    <div>
+      <h1>Survey</h1>
+      <div className="InfoBox">
+        <h3>Here's the criteria</h3>
+        <p>
+          You are a message analysis assistant. Your task is to evaluate whether a given message contains coded language that targets, incites, or supports military action, armed revolt, or civil war against specific people, persons, or organizations. Use the following rating scale for your evaluation:
+
+          When you receive a message, analyze its content and reply ONLY with the final rating number.
+        </p>
+
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          <li>1: No coded language</li>
+          <li>2: Possible coded language</li>
+          <li>3: Coded language</li>
+          <li>4: Strong coded language</li>
+          <li>5: Extreme coded language</li>
+        </ul>
+        <h3>Please rate the following message:</h3>
+      </div>
+      <input type="radio" id="1" name="rank" value="1" />
+      <label htmlFor="1">1</label>
+      <input type="radio" id="2" name="rank" value="2" />
+      <label htmlFor="2">2</label>
+      <input type="radio" id="3" name="rank" value="3" />
+      <label htmlFor="3">3</label>
+      <input type="radio" id="4" name="rank" value="4" />
+      <label htmlFor="4">4</label>
+      <input type="radio" id="5" name="rank" value="5" />
+      <label htmlFor="5">5</label>
+      <button onClick={submit}>Submit</button>
+    </div>
+  );
+}
+export default Survey;
+
