ea8626d6d9003d02f5d42580a7efed367b51d6a3
cam-ucf <ca840220@ucf.edu>
2025-03-07 17:12:04 -0500

    created hot reloading next.js docker container and compose

--- HomeLandSec/frontend/src/app/page.tsx
+++ HomeLandSec/frontend/src/app/page.tsx
@@ -22,7 +22,9 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <p className="text-sm text-center sm:text-left">
+          Happy coding! 🎉
+        </p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
