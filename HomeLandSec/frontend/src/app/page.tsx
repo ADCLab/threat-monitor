f6395ff49f622edaf8c837b4ad62a502e097816b
cam-ucf <ca840220@ucf.edu>
2025-03-07 18:21:34 -0500

    create flexible button components

--- HomeLandSec/frontend/src/app/page.tsx
+++ HomeLandSec/frontend/src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import Image from "next/image";
+import Button from "@/components/ui/button";
 
 export default function Home() {
+  function testButton() {
+    console.log("Button clicked!");
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -18,13 +24,16 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
-            .
+
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
         <p className="text-sm text-center sm:text-left">
           Happy coding! 🎉
         </p>
+
+        <Button type="button" onClick={() => { console.log("onClick!") }}>onClick method button</Button>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
