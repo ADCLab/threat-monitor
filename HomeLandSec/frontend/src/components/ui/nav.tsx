80959dc6d37229be7bc8f7fdacf74ddac02fff2d
cam-ucf <ca840220@ucf.edu>
2025-03-07 19:30:15 -0500

    Add nav bar and radio buttons

---
+++ HomeLandSec/frontend/src/components/ui/nav.tsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+	return (
+		<nav className="flex items-center p-2 bg-gray-800 text-white">
+			<img src="adcLab.png" alt="Logo" className="h-16 pr-2" />
+			<h1 className="text-2xl font-bold">ADC Lab</h1>
+		</nav>
+	);
+}
